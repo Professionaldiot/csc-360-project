1a6656c3ac59e55580f46532d184a613f6276a0d
ajs541 <ajstand@live.com>
2024-11-01 17:14:02 -0500

    SWitched around login with Card and other Material UI elements.  Added a nice box around login too.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,59 +4,58 @@ import Box from '@mui/material/Box'
 import TextField from '@mui/material/TextField';
 import { createTheme } from '@mui/material';
 import Button from '@mui/material/Button';
+import { Card, CardActions, CardContent, Typography } from '@mui/material';
 
 // main function, runs the HTML stuff
 function App() {
   return (
     <>
-    {/*Header business here*/}
+      {/*Header business here*/}
       <div className='headContainer'>
         <div className='selfService-Header'>
           <img src={"https://www.cornellcollege.edu/assets/production/images/cornell_logo_white.png"} className="cornell-logo" alt="logo" />
         </div>
         <div className='subHeader'>
           <p>Courses</p>
-          </div>
+        </div>
         <div className='selfService-Header'>
           <p>Login</p>
         </div>
       </div>
 
-        {/*All login stuff in this <div>*/}
+      {/*All login stuff in this <div>*/}
       <div className='loginBack'>
-        <div className='loginBox'>
-          <h1>Login</h1>
+        <Box sx={{justifyContent: 'space-around', alignItems: 'center', position: "absolute", top: '50px'}}>
+          <Card sx={{ backgroundColor: '#00000025', height: '310px' }}>
+            <CardContent>
+              <Typography variant="h2" component="div" sx={{ textAlign: "center"}}>
+                Login
+              </Typography>
+              <TextField
+                id="filled-required"
+                label="Username"
+                variant="filled"
+              />
+              <br/>
+              <br/>
+              <TextField
+                id="filled-password-input"
+                label="Password"
+                type="password"
+                autoComplete="current-password"
+                variant="filled"
+              />
+            </CardContent>
+            <CardActions sx={{ alignItems: "center", justifyContent: "space-around"}}>
+              <Button variant="outlined">Login</Button>
+              <Button variant="outlined">Guest</Button>
+            </CardActions>
 
-          {/*Username Text Field*/}
-          <div className='textField'>
-            <TextField  
-              required
-              id="filled-required"
-              label="Username"
-              variant="filled"
-            />   
-          </div>
-          <br/>
+            {/*Username Text Field*/}
 
-          {/*Password Text Field*/}        
-          <div className='textField'>
-            <TextField
-              id="filled-password-input"
-              label="Password"
-              type="password"
-              autoComplete="current-password"
-              variant="filled"
-            />
-          </div>
-          <br/>
 
-          {/*Login/Guest Buttons*/}  
-          <div className='textField'>
-            <Button variant="outlined">Login</Button>
-            <Button variant="outlined">Guest</Button>
-          </div>      
-
-        </div>
+          </Card>
+        </Box>
       </div>
     </>
   );
