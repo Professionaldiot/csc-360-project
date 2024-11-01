a0ed749688630e7f2babbfee80fdf256a94f83a1
Willow Dennison <dennisonwillow@gmail.com>
2024-11-01 14:05:16 -0500

    API to recieve username and password packet and validate login info, plus table of login info for test purposes

---
+++ back-end/passwordValidatorAPI.py
@@ -0,0 +1,37 @@
+from flask import Flask, request
+import mysql.connector
+
+app = Flask(__name__)
+
+#to be filled with server connection info, was configured for localhost
+db = mysql.connector.connect(
+    host = "",
+    user = "",
+    password = "",
+    database = "SelfService"
+)
+
+cursor = db.cursor()
+
+#Recieves JSON packet containing username and password from login page, returns a boolean value based on if the login information is valid or not
+#retrieves passcode from MySQL database based on username and checks the provided password against it
+@app.post("/validate")
+def validateLogin():
+
+    data = request.get_json()
+    username = data.get("username")
+    password = data.get("password")
+
+    cursor.execute("SELECT (passcode) FROM users WHERE user_name = (%s);", (username,))
+    result = cursor.fetchone()
+    
+    try:
+        if password == result[0]:
+            return {"success": True}
+        
+    except:
+        return {"success": False}
+        
+    return {"success": False}
+
+app.run()
