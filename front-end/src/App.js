1f6a9fe08927307b0eca3596675add19c1590753
Declan <redstonemaster79@gmail.com>
2024-11-04 10:50:17 -0600

    added helper text below username and password box, and also made the grey box wider

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
-import Box from '@mui/material/Box'
+import * as React from 'react';
+import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import { createTheme } from '@mui/material';
+import { createTheme, FormHelperText } from '@mui/material';
+import FormControl, {useFormControl} from '@mui/material/FormControl';
 import Button from '@mui/material/Button';
 import { Card, CardActions, CardContent, Typography } from '@mui/material';
+import FilledInput from '@mui/material/FilledInput';
+import InputLabel from '@mui/material/InputLabel';
+
+
+function EmptyUser() {
+  /*
+  this function is using materialUI's value.filled
+  from the FormControl import to check whether the
+  input on the box it's in is filled, it will only
+  work for the specific box it's in
+  */
+  const { filled } = useFormControl() ;
+
+  const helperText = React.useMemo(() =>{
+    if (filled) {
+      return '';
+    }
+    return "Please enter a username.";
+  },[filled]);
+
+  return <FormHelperText>{helperText}</FormHelperText>;
+}
+
+
+function EmptyPassword() {
+  /*
+  same function as above, but done for the password
+  */
+  const { filled } = useFormControl() ;
+
+  const helperText = React.useMemo(() =>{
+    if (filled) {
+      return '';
+    }
+    return "Please enter a password.";
+  },[filled]);
+
+  return <FormHelperText>{helperText}</FormHelperText>;
+}
 
 // main function, runs the HTML stuff
 function App() {
@@ -13,7 +53,11 @@ function App() {
       {/*Header business here*/}
       <div className='headContainer'>
         <div className='selfService-Header'>
-          <img src={"https://www.cornellcollege.edu/assets/production/images/cornell_logo_white.png"} className="cornell-logo" alt="logo" />
+          <img
+            //cornell logo
+            src={"https://www.cornellcollege.edu/assets/production/images/cornell_logo_white.png"} 
+            className="cornell-logo" 
+            alt="logo" />
         </div>
         <div className='subHeader'>
           <p>Courses</p>
@@ -26,34 +70,37 @@ function App() {
       {/*All login stuff in this <div>*/}
       <div className='loginBack'>
         <Box sx={{ justifyContent: 'space-around', alignItems: 'center', position: "absolute", top: '50px' }}>
-          <Card sx={{ backgroundColor: '#00000025', height: '310px' }}>
+          <Card sx={{ backgroundColor: '#00000025', height: '350px', width: '375px'}}>
             <CardContent>
-              <Typography variant="h2" component="div" sx={{ textAlign: "center" }}>
+              <Typography variant="h3" component="div" sx={{ textAlign: "center"}}>
                 Login
               </Typography>
-              <TextField
-                id="filled-required"
-                label="Username"
-                variant="filled"
-              />
-              <br />
-              <br />
-              <TextField
-                id="filled-password-input"
-                label="Password"
-                type="password"
-                autoComplete="current-password"
-                variant="filled"
-              />
+                {/*Username Text Field*/}
+                <div className='textField'>
+                  <FormControl>
+                    <InputLabel htmlFor="filled-adornment-password">Username</InputLabel>
+                      <FilledInput
+                        id="login-username"
+                      />
+                    <EmptyUser />
+                  </FormControl>
+                    <br />
+                  <FormControl>
+                    <InputLabel htmlFor="filled-adornment-password">Password</InputLabel>
+                      <FilledInput
+                        id="login-password"
+                        type='password'
+                      />
+                    <EmptyPassword />
+                  </FormControl>
+                </div>
             </CardContent>
-            <CardActions sx={{ alignItems: "center", justifyContent: "space-around" }}>
-              <Button variant="outlined">Login</Button>
-              <Button variant="outlined">Guest</Button>
-            </CardActions>
-
-            {/*Username Text Field*/}
-
-
+            <div className='textField'>
+              <CardActions sx={{ alignItems: "center", justifyContent: "space-around"}}>
+                <Button variant="outlined">Login</Button>
+                <Button variant="outlined">Guest</Button>
+              </CardActions>
+            </div>
           </Card>
         </Box>
       </div>
