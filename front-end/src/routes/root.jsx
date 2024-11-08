2cfab593cc3002a627336df805fc88150bd7adca
ajs541 <ajstand@live.com>
2024-11-08 14:15:16 -0600

    Added UserContext to root so all the other pages can access the login state and userData

--- front-end/src/routes/root.jsx
+++ front-end/src/routes/root.jsx
@@ -1,9 +1,10 @@
 import './App.css';
 import * as React from 'react';
+import { useEffect, useState, createContext } from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { createTheme, FormHelperText } from '@mui/material';
-import FormControl, {useFormControl} from '@mui/material/FormControl';
+import FormControl, { useFormControl } from '@mui/material/FormControl';
 import Button from '@mui/material/Button';
 import { Card, CardActions, CardContent, Typography } from '@mui/material';
 import FilledInput from '@mui/material/FilledInput';
@@ -13,28 +14,38 @@ import { Outlet, Link } from 'react-router-dom'
 import Menu from '@mui/material/Menu';
 
 
+export const UserContext = createContext(null);
+
 export default function Root() {
-    return(
+    const [isLogged, setIsLogged] = useState(false);
+    const [userData, setUserData] = useState([]);
+
+    return (
         <>
-            {/*Header business here*/}
-            <div className='headContainer'>
-                <div className='selfService-Header'>
-                <img
-                    //cornell logo
-                    src={"https://www.cornellcollege.edu/assets/production/images/cornell_logo_white.png"} 
-                    className="cornell-logo" 
-                    alt="logo" />
+            <UserContext.Provider value={{
+                isLogged: isLogged, setIsLogged: setIsLogged,
+                userData: userData, setUserData: setUserData,
+            }} >
+                {/*Header business here*/}
+                <div className='headContainer'>
+                    <div className='selfService-Header'>
+                        <img
+                            //cornell logo
+                            src={"https://www.cornellcollege.edu/assets/production/images/cornell_logo_white.png"}
+                            className="cornell-logo"
+                            alt="logo" />
+                    </div>
+                    <div className='subHeader'>
+                        <Button onClick={() => { window.location.href = "/courses"; }}>Courses</Button>
+                    </div>
+                    <div className='selfService-Header'>
+                        <Link to={'login'}><Avatar alt="Login" sx={{ width: 56, height: 56 }} /></Link>
+                    </div>
+                    <div id='detail'>
+                        <Outlet />
+                    </div>
                 </div>
-                <div className='subHeader'>
-                    <Button onClick={() => {window.location.href = "/courses";}}>Courses</Button>
-                </div>
-                <div className='selfService-Header'>
-                    <Link to={'login'}><Avatar alt="Login" sx = {{width: 56, height: 56}}/></Link>
-                </div>
-                <div id='detail'>
-                    <Outlet/>
-                </div>
-            </div>
+            </UserContext.Provider>
         </>
     )
 }
