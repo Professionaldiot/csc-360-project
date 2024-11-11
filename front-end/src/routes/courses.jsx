38de2898e866fd218356bf49cbf6c8c37177c5a0
gcarm25 <gcarmichael25@cornellcollege.edu>
2024-11-11 15:37:50 -0600

    Working further on courses page, might work now

--- front-end/src/routes/courses.jsx
+++ front-end/src/routes/courses.jsx
@@ -31,31 +31,31 @@ const Item = styled(Paper)(({ theme }) => ({
     }),
 }));
 
-function testFetchCourses(search, blockNum, department) {
+function TestFetchCourses(search, blockNum, department) {
     if (department === 1) {
-        return [{course_code: 'CS101', course_name: 'Introduction to Computer Science', block_num: 'B1', course_description: 'Fundamentals of computer science, including algorithms and data structures.', department_id: 1, faculty_id: 21},
+        return ([{course_code: 'CS101', course_name: 'Introduction to Computer Science', block_num: 'B1', course_description: 'Fundamentals of computer science, including algorithms and data structures.', department_id: 1, faculty_id: 21},
         {course_code: 'CS102', course_name: 'Data Structures', block_num: 'B2', course_description: 'Introduction to data organization techniques for efficient data processing.', department_id: 1, faculty_id: 21},
-        {course_code: 'CS202', course_name: 'Computer Architecture', block_num: 'B3', course_description: 'Introduction to computer hardware and low-level programming.', department_id: 1, faculty_id: 21}]
+        {course_code: 'CS202', course_name: 'Computer Architecture', block_num: 'B3', course_description: 'Introduction to computer hardware and low-level programming.', department_id: 1, faculty_id: 21}])
     }
     else if (department === 2) {
-        return [{course_code: 'MAT101', course_name: 'Calculus I', block_num: 'B1', course_description: 'Introduction to differential and integral calculus.', department_id: 2, faculty_id: 22},
-        {course_code: 'MAT102', course_name: 'Linear Algebra', block_num: 'B2', course_description: 'Introduction to vector spaces and matrix operations.', department_id: 2, faculty_id: 22}]
+        return ([{course_code: 'MAT101', course_name: 'Calculus I', block_num: 'B1', course_description: 'Introduction to differential and integral calculus.', department_id: 2, faculty_id: 22},
+        {course_code: 'MAT102', course_name: 'Linear Algebra', block_num: 'B2', course_description: 'Introduction to vector spaces and matrix operations.', department_id: 2, faculty_id: 22}])
     }
     else if (department === 3) {
-        return [{course_code: 'PHY101', course_name: 'Physics I', block_num: 'B1', course_description: 'Fundamentals of mechanics and thermodynamics.', department_id: 3, faculty_id: 23},
+        return ([{course_code: 'PHY101', course_name: 'Physics I', block_num: 'B1', course_description: 'Fundamentals of mechanics and thermodynamics.', department_id: 3, faculty_id: 23},
         {course_code: 'PHY102', course_name: 'Physics II', block_num: 'B2', course_description: 'Introduction to electricity and magnetism.', department_id: 3, faculty_id: 23},
-        {course_code: 'PHY201', course_name: 'Quantum Mechanics', block_num: 'B3', course_description: 'Introduction to quantum theory.', department_id: 3, faculty_id: 23}]
+        {course_code: 'PHY201', course_name: 'Quantum Mechanics', block_num: 'B3', course_description: 'Introduction to quantum theory.', department_id: 3, faculty_id: 23}])
     }
     else if (department === 4) {
-        return [{course_code: 'BIO201', course_name: 'Genetics', block_num: 'B3', course_description: 'Introduction to Mendelian and molecular genetics.', department_id: 4, faculty_id: 24},
-        {course_code: 'BIO202', course_name: 'Microbiology', block_num: 'B4', course_description: 'Study of microorganisms and their roles in the environment.', department_id: 4, faculty_id: 24}]
+        return ([{course_code: 'BIO201', course_name: 'Genetics', block_num: 'B3', course_description: 'Introduction to Mendelian and molecular genetics.', department_id: 4, faculty_id: 24},
+        {course_code: 'BIO202', course_name: 'Microbiology', block_num: 'B4', course_description: 'Study of microorganisms and their roles in the environment.', department_id: 4, faculty_id: 24}])
     }
     else if (department === 5) {
-        return [{course_code: 'CHE101', course_name: 'General Chemistry I', block_num: 'B1', course_description: 'Fundamentals of chemistry including atomic theory and bonding.', department_id: 5, faculty_id: 25},
-        {course_code: 'CHE102', course_name: 'Organic Chemistry', block_num: 'B2', course_description: 'Introduction to organic compounds and reactions.', department_id: 5, faculty_id: 25}]
+        return ([{course_code: 'CHE101', course_name: 'General Chemistry I', block_num: 'B1', course_description: 'Fundamentals of chemistry including atomic theory and bonding.', department_id: 5, faculty_id: 25},
+        {course_code: 'CHE102', course_name: 'Organic Chemistry', block_num: 'B2', course_description: 'Introduction to organic compounds and reactions.', department_id: 5, faculty_id: 25}])
     }
     else {
-        return [{course_code: 'CS101', course_name: 'Introduction to Computer Science', block_num: 'B1', course_description: 'Fundamentals of computer science, including algorithms and data structures.', department_id: 1, faculty_id: 21},
+        return ([{course_code: 'CS101', course_name: 'Introduction to Computer Science', block_num: 'B1', course_description: 'Fundamentals of computer science, including algorithms and data structures.', department_id: 1, faculty_id: 21},
         {course_code: 'CS102', course_name: 'Data Structures', block_num: 'B2', course_description: 'Introduction to data organization techniques for efficient data processing.', department_id: 1, faculty_id: 21},
         {course_code: 'CS202', course_name: 'Computer Architecture', block_num: 'B3', course_description: 'Introduction to computer hardware and low-level programming.', department_id: 1, faculty_id: 21},
         {course_code: 'MAT101', course_name: 'Calculus I', block_num: 'B1', course_description: 'Introduction to differential and integral calculus.', department_id: 2, faculty_id: 22},
@@ -66,9 +66,32 @@ function testFetchCourses(search, blockNum, department) {
         {course_code: 'BIO201', course_name: 'Genetics', block_num: 'B3', course_description: 'Introduction to Mendelian and molecular genetics.', department_id: 4, faculty_id: 24},
         {course_code: 'BIO202', course_name: 'Microbiology', block_num: 'B4', course_description: 'Study of microorganisms and their roles in the environment.', department_id: 4, faculty_id: 24},
         {course_code: 'CHE101', course_name: 'General Chemistry I', block_num: 'B1', course_description: 'Fundamentals of chemistry including atomic theory and bonding.', department_id: 5, faculty_id: 25},
-        {course_code: 'CHE102', course_name: 'Organic Chemistry', block_num: 'B2', course_description: 'Introduction to organic compounds and reactions.', department_id: 5, faculty_id: 25}]
+        {course_code: 'CHE102', course_name: 'Organic Chemistry', block_num: 'B2', course_description: 'Introduction to organic compounds and reactions.', department_id: 5, faculty_id: 25}])
     }
 }
+
+
+// function TestFetchCourses(search, blockNum, department) {
+//     if (department === 1) {
+//         return ([{course_code: 'CS101', course_name: 'Introduction to Computer Science', block_num: 'B1', course_description: 'Fundamentals of computer science, including algorithms and data structures.', department_id: 1, faculty_id: 21}])
+//     }
+//     else if (department === 2) {
+//         return ([{course_code: 'MAT101', course_name: 'Calculus I', block_num: 'B1', course_description: 'Introduction to differential and integral calculus.', department_id: 2, faculty_id: 22}])
+//     }
+//     else if (department === 3) {
+//         return ([{course_code: 'PHY101', course_name: 'Physics I', block_num: 'B1', course_description: 'Fundamentals of mechanics and thermodynamics.', department_id: 3, faculty_id: 23}])
+//     }
+//     else if (department === 4) {
+//         return ([{course_code: 'BIO201', course_name: 'Genetics', block_num: 'B3', course_description: 'Introduction to Mendelian and molecular genetics.', department_id: 4, faculty_id: 24}])
+//     }
+//     else if (department === 5) {
+//         return ([{course_code: 'CHE101', course_name: 'General Chemistry I', block_num: 'B1', course_description: 'Fundamentals of chemistry including atomic theory and bonding.', department_id: 5, faculty_id: 25}])
+//     }
+//     else {
+//         return ([{course_code: 'CS101', course_name: 'Introduction to Computer Science', block_num: 'B1', course_description: 'Fundamentals of computer science, including algorithms and data structures.', department_id: 1, faculty_id: 21}])
+//     }
+// }
+
 
 
 
@@ -86,10 +109,15 @@ export default function Courses() {
         setBlock(event.target.value);
     };
 
+    const updateCourses = () => {setCourseList(TestFetchCourses("", block, dept))
+        console.log(courseList) 
+    };
+
 //    let courseList = [{courseCode: 'Bio10101', courseName: 'Biology Of Your Eyes', blockNum: 'B2', courseDescription: 'Please Work'}];
     let testingVariable = 'Unchanged';
 
-    const [courseList, setCourseList] = React.useState([]);
+    const [courseList, setCourseList] = React.useState([{}]);
+//    console.log(TestFetchCourses("", 'b1', 1))
 
     return (
         <>
@@ -106,10 +134,7 @@ export default function Courses() {
                                 variant="filled"
                                 sx={{ width: '800px' }}
                             />
-                            <Button variant='contained' onClick={
-                                () => {setCourseList(testFetchCourses("", block, dept))
-                                    console.log(courseList)
-                                }
+                            <Button variant='contained' onClick= {()=>{updateCourses()}
                                 
                         } sx={{ height: '8vmin', position: 'absolute', float: 'right', marginLeft: '12px', backgroundColor: '#7c2bb3' }}>Search</Button>
                         </CardContent>
@@ -169,15 +194,28 @@ export default function Courses() {
                     <br />
                     
                     <Stack spacing={2} sx={{ float: 'left', marginLeft: '48px', width: '950px', textAlign: 'left' }}>
+                    
+                    {courseList.length > 0 ? (
+                        courseList.map((course, index) => {
+                            return (
+                                <Item sx={{height: '64px'}}>{course.course_code}:{course.course_name}</Item>
+                            )
+                        })
+                        )
+                        :
+                        (<p>No Course to Display</p>)
+                    }
                         
-                        <Item sx={{ height: '28px' }}>{courseList[0]}</Item>
-                        <Item sx={{ height: '64px' }}>{testingVariable + ": Is this Working?"}</Item>
-                        <Item sx={{ height: '64px' }}>{courseList[3]}</Item>
+
+                        <Item sx={{ height: '28px' }}>{courseList[0].course_code + ": " + courseList[0].course_name}</Item>
+                        <Item sx={{ height: '64px' }}>{": Is this Working?"}</Item>
+                        {console.log(courseList)}
+                        {/* <Item sx={{ height: '64px' }}>{courseList[3]}</Item>
                         <Item sx={{ height: '64px' }}>{courseList[4]}</Item>
                         <Item sx={{ height: '64px' }}>{courseList[5]}</Item>
                         <Item sx={{ height: '64px' }}>{courseList[6]}</Item>
                         <Item sx={{ height: '64px' }}>{courseList[7]}</Item>
-                        <Item sx={{ height: '64px' }}>{courseList[8]}</Item>
+                        <Item sx={{ height: '64px' }}>{courseList[8]}</Item> */}
                     </Stack>
                 </Box>
             </div>
