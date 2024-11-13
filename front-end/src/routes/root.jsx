c42ae99af50e3219e36e7875e66a4245b0021a44
ajs541 <ajstand@live.com>
2024-11-12 18:21:27 -0600

    Fixed small syntax issue in registerstatus.  Cleaned up code in root so that buttons specific to certain roles only show up for said roles (as its own function)

--- front-end/src/routes/root.jsx
+++ front-end/src/routes/root.jsx
@@ -5,74 +5,53 @@ import Avatar from '@mui/material/Avatar';
 import { Outlet, Link } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom';
 import { useGlobalState } from '../functions/globalState';
-
-/*
-const ShowForUser = (role) => {
-    const navigate = useNavigate();
-    if (role === "student" || role === "faculty") {
-        return(
-            <Button onClick={() => {navigate("/schedule")}}>Schedule</Button>
-        )
-    } else return(null)
-}
-    */
-
-
+import { Fragment } from 'react';
 
 export default function Root() {
     const navigate = useNavigate();
     const { userData, setUserData, isLogged, setIsLogged } = useGlobalState();
 
-    if (isLogged) {
-        return (
-            <>
-                {/*Header business here*/}
-                <div className='headContainer'>
-                    <div className='selfService-Header'>
-                        <img
-                            //cornell logo
-                            src={"https://www.cornellcollege.edu/assets/production/images/cornell_logo_white.png"}
-                            className="cornell-logo"
-                            alt="logo" />
-                    </div>
-                    <div className='subHeader'>
-                        <Button onClick={() => { navigate("/courses") }}>Courses</Button>
+    const ScheduleAndRegistration = () => {
+        if (isLogged) {
+            if (userData.userType === "Student") {
+                return (
+                    <Fragment>
                         <Button onClick={() => { navigate("/schedule") }}>Schedule</Button>
                         <Button onClick={() => { navigate("/registration") }}>Registration</Button>
-                    </div>
-                    <div className='selfService-Header'>
-                        <Link to={'schedule'}><Avatar alt="Schedule" sx={{ width: 56, height: 56 }} /></Link>
-                    </div>
-                    <div id='detail'>
-                        <Outlet />
-                    </div>
-                </div>
-            </>
-        )
+                    </Fragment>
+                )
+            } if (userData.userType === "Faculty") {
+                return (
+                    <Fragment>
+                        <Button onClick={() => { navigate("/schedule") }}>Schedule</Button>
+                    </Fragment>
+                )
+            }
+        }
     }
-    else {
-        return (
-            <>
-                {/*Header business here*/}
-                <div className='headContainer'>
-                    <div className='selfService-Header'>
-                        <img
-                            //cornell logo
-                            src={"https://www.cornellcollege.edu/assets/production/images/cornell_logo_white.png"}
-                            className="cornell-logo"
-                            alt="logo" />
-                    </div>
-                    <div className='subHeader'>
-                        <Button onClick={() => { navigate("/courses") }}>Courses</Button>
-                    </div>
-                    <div className='selfService-Header'>
-                        <Link to={'login'}><Avatar alt="Login" sx={{ width: 56, height: 56 }} /></Link>
-                    </div>
-                    <div id='detail'>
-                        <Outlet />
-                    </div>
+    
+    return (
+        <>
+            {/*Header business here*/}
+            <div className='headContainer'>
+                <div className='selfService-Header'>
+                    <img
+                        //cornell logo
+                        src={"https://www.cornellcollege.edu/assets/production/images/cornell_logo_white.png"}
+                        className="cornell-logo"
+                        alt="logo" />
                 </div>
-            </>
-        )
-    }
+                <div className='subHeader'>
+                    <Button onClick={() => { navigate("/courses") }}>Courses</Button>
+                    <ScheduleAndRegistration />
+                </div>
+                <div className='selfService-Header'>
+                    <Link to={'schedule'}><Avatar alt="Schedule" sx={{ width: 56, height: 56 }} /></Link>
+                </div>
+                <div id='detail'>
+                    <Outlet />
+                </div>
+            </div>
+        </>
+    )
 }
